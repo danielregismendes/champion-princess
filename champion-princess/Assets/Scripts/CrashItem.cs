bd1ebb4b89ca32545dd28f0c6e70d0d5a294ab1f
Daniel Mendes <80532809+danielregismendes@users.noreply.github.com>
2024-02-27 19:17:26 -0300

    Alfa 0.008
    Drops configurados nos tambores quebrados.

--- champion-princess/Assets/Scripts/CrashItem.cs
+++ champion-princess/Assets/Scripts/CrashItem.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 [Serializable]
 public class CrashItem : MonoBehaviour
 {
+    public GameObject dropItem;
+
     private int currentDamage = 0;
     private Animator anim;
+    private Transform transform;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
+        transform = GetComponent<Transform>();
     }
 
     public void TookDamage(int damage)
@@ -25,6 +29,9 @@ public class CrashItem : MonoBehaviour
 
     public void DestroyItem()
     {
+        Instantiate(dropItem, transform.position, transform.rotation);
         gameObject.SetActive(false);
     }
+
+
 }
